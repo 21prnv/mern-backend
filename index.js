e0a80fd9654d09c399c996af0f35f1a7588050ee
21prnv <pranavpatil6251@gmail.com>
2023-02-14 19:56:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const port = process.env.port || 3000;
+
+const app = express();
+const bodyParser = require("body-parser");
+//
+require("./db");
+require("./models/User");
+//
+const authRoutes = require("./routs/authRoute");
+const requireToken = require("./Middelwares/AuthTokenRequir");
+//
+app.use(bodyParser.json());
+app.use(authRoutes);
+//
+
+app.get("/", requireToken, (req, res) => {
+  res.send("This is home page");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.pre("save", async function (next) {
+  const user = this;
+  console.log("before saving", user.password);
+  if (!user.isModified("password")) {
+    return next();
+  }
+  user.password = await bcrypt.hash(user.password, 8);
+  console.log("after saving", user.password);
+  next();
+});
+mongoose.model("User", userSchema);
